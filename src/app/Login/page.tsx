5fa5fd9cf88ab5676a45824e801dcd5a043cbaae
bcarolm <carolina.burgum@gmail.com>
2024-09-03 20:29:20 -0300

    feat: Background da Tela de Login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,7 +1,12 @@
 export default function Login() {
     return(
-        <div>
-            <text>Hello</text>
+        <div className="h-screen flex">
+            <div className="w-1/2 h-screen bg-gradient-to-r from-darkBlue to-lightBlue">
+                
+            </div>
+            <div className="w-1/2 h-screen bg-white">
+
+            </div>
         </div>
     );
 }
