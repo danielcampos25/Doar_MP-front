d3f13c76ed596814aa2e1ece6adfa0f0ac1b716e
bcarolm <carolina.burgum@gmail.com>
2024-09-03 20:45:43 -0300

    feat: Logo tela login

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,8 +1,12 @@
+import logo from "../../../public/Doar.com 1.svg";
+import Image from "next/image";
+
 export default function Login() {
     return(
         <div className="h-screen flex">
-            <div className="w-1/2 h-screen bg-gradient-to-r from-darkBlue to-lightBlue">
-                
+            <div className="w-1/2 h-screen bg-gradient-to-r from-darkBlue to-lightBlue flex flex-col justify-center items-center">
+                <Image src={logo} alt="Logo" width={400} height={400} />
+                <h1 className="text-9xl text-white font-questrial">do ar.com</h1>
             </div>
             <div className="w-1/2 h-screen bg-white">
 
