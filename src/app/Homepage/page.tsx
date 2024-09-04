60ae09736867552f73829121e16a50506383f234
felipe gibin <gibinf.duarte@gmail.com>
2024-09-04 11:54:49 -0300

    feat tela concluida

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -20,6 +20,29 @@ export default function Homepage() {
         </div>
       </header>
 
+      <main className="flex-grow flex items-center justify-between px-40">
+        <div className="text-white font-questrial text-7xl max-w-[650px] max-h-[592px]">
+          <p>Doar nunca foi tão fácil e seguro.</p>
+        </div>
+
+        <div className="relative w-[500px] h-[600px] mt-5 mr-4">
+          <div className="absolute top-0 right-0">
+            <Image src="/Map-homepage.svg" alt="Map" width={381} height={418} />
+          </div>
+          <div className=" absolute right-0 bottom-4">
+            <Image src="/Box-homepage.svg" alt="Box" width={153} height={164} />
+          </div>
+          <div className="absolute bottom-16 left-0">
+            <Image
+              src="/Truck-homepage.svg"
+              alt="Truck"
+              width={332}
+              height={325}
+            />
+          </div>
+        </div>
+      </main>
+
       <footer className="absolute bottom-0 w-full h-[102px] bg-softGray bg-opacity-50 flex items-center">
         <h2 className="font-questrial text-2xl ml-36 text-softBlack">
           do_ar2024@gmail.com
@@ -44,7 +67,7 @@ export default function Homepage() {
             width={61}
             height={61}
           />
-        </div> 
+        </div>
       </footer>
     </div>
   );
