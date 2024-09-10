103703f49fe6b86ce55e96aaad920b93f0e65939
bg1777 <bgrodrigues2005@gmail.com>
2024-09-10 16:15:52 -0300

    fix: consertando cagadas do ultimo commit

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useRef } from "react";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import logo from "../../../public/Doar.com 1.svg";
 import pfp from "../../../public/pfp.svg";
@@ -16,7 +15,6 @@ export default function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [isInstitution, setIsInstitution] = useState(false);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const router = useRouter();
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -47,16 +45,8 @@ export default function Login() {
         senha: values.senha,
         userType: values.userType,
       });
-      
-      const { access_token } = response.data; // assuming access_token is in response.data
-  
-      // Save the token in localStorage
-      localStorage.setItem("access_token", access_token);
-  
+      console.log(response.data);
       alert("Login successful!");
-  
-      // Redirect to feed
-      router.push('/Feed');
     } catch (error) {
       console.error(error);
       alert("Login failed. Please check your credentials.");
